ede5c215aedb67243771ab54e11a6739fe909951
Adonaï Elohim Exaucé <adonainangui03@gmail.com>
2019-05-13 01:33:59 +0000

    Cors middleware added

--- resources/js/components/Project.vue
+++ resources/js/components/Project.vue
@@ -1,7 +1,35 @@
 <template>
-    <div>Project component</div>
+    <div>
+        <h1>Projects stats</h1>
+        <p>Projects: {{ projects }}</p>
+        <p>Unit price: {{ cost }}</p>
+    </div>
 </template>
 
 <script>
-export default {};
+export default {
+    data() {
+        return {
+            projects: 0,
+            cost: 0
+        };
+    },
+    created() {
+        axios.get('http://localhost:8000/api/tests')
+            // You can use this
+            // .then(({ data }) => {
+            //     let { projects, cost } = data;
+
+            //     this.projects = data.projects;
+            //     this.cost = data.unit_cost;
+            // });
+            .then(response => response.data)
+            .then(data => {
+                let { projects, cost } = data;
+
+                this.projects = data.projects;
+                this.cost = data.unit_cost;
+            });
+    }
+};
 </script>
--- resources/js/routes.js
+++ resources/js/routes.js
@@ -2,6 +2,12 @@ let Home = () => import(/* webpackChunkName: "loaders" */'./components/Home');
 import Project from './components/Project';
 import NotFound from './components/NotFound';
 
+import axios from 'axios';
+
+window.axios = axios;
+window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+
+
 export default {
     mode: 'history',
     linkActiveClass: 'font-bold',
